e814a5f6cd11f6b5a1e6282d318c60a1613b8869
KeVT <83045419+KeVT@users.noreply.github.com>
2021-10-24 02:15:23 -0500

    Update every_other.java
    Se implementó la función every_other donde dado un arreglo de números enteros, la función sumará el número de orden par del array con todos los números de dicho array.
    El algoritmo tiene una complejidad de O(n^2).

--- Laboratorio 4/every_other.java
+++ Laboratorio 4/every_other.java
@@ -1,19 +1,19 @@
-import java.util.*;
-public class every_other {
+//every_other.java
 
+import java.util.*;
+public class every_other {		  
 	public static void main(String[] args) {	
-    //caso de prueba
 		int [] array = {1,2,3};	
     System.out.println(Arrays.toString(array));
 		System.out.println();
 		every_other(array);
 	}
-	//método que sumará el número de orden par de un array con todos los números de dicho array.
-	public static void every_other(int [] array) {      
-    for(int i=0;i<array.length;i++){       	        
-      if (i % 2 == 0) {     		
-        for (int j = 0; j < array.length;j++)       			
-          System.out.println(array[i] + " + " + array[j] + " = " + (array[i]+array[j]));      		        	
+	public static void every_other(int [] array) {    
+		//dado un arreglo de números enteros, la función sumará el número de orden par del array con todos los números de dicho array.
+    for(int i=0;i<array.length;i++){    																									  //---O(n^2)   	        
+      if (i % 2 == 0) {     																																//---O(6n)									
+        for (int j = 0; j < array.length;j++)       			                                  //---O(n)		
+          System.out.println(array[i] + " + " + array[j] + " = " + (array[i]+array[j]));    //---O(6)		 			        	
       }        
     }        	
   }
