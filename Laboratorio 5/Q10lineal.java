577b9bff9b3f1c4b997aede39993423046fa4f42
KeVT <83045419+KeVT@users.noreply.github.com>
2021-10-30 18:49:22 -0500

    Create Q10lineal.java
    Dado un número, se recorren dos ciclos separados  y cada ciclo muestra valores consecutivos hasta antes de dicho número.
    Tiene una complejidad computacional de O(n).

---
+++ Laboratorio 5/Q10lineal.java
@@ -0,0 +1,18 @@
+//Q10lineal.java
+
+public class Q10lineal {
+
+	public static void main(String[] args) {	
+		//Dado un número, se recorren dos ciclos separados 
+    //y cada ciclo muestra valores consecutivos hasta antes de dicho número.
+		int n = 10;
+		for(int i = 0; i < n; i++) {		//O((n))	 --> 	O(n) + O(n) = O(2n) --> O(n)
+			System.out.println(i);
+		}                               //  +
+		System.out.println(); 
+		for(int j = 0; j < n; j++) {		//O((n))	
+			System.out.println(j);
+		}		
+	}
+	
+}
