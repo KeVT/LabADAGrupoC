4f52ec443112a9d0acd1c6270eb15af969f164a0
KeVT <83045419+KeVT@users.noreply.github.com>
2021-10-30 18:15:55 -0500

    Create Q2lineal.java
    Dado un número, se imprimen números consecutivos descendientemente desde ese número hasta el 1.
    Tiene una complejidad computacional de O(n).

---
+++ Laboratorio 5/Q2lineal.java
@@ -0,0 +1,26 @@
+//Q2lineal.java
+
+public class Q2lineal {
+
+	public static void main(String[] args) {	
+		//Dado un número, se imprimen números consecutivos descendientemente desde ese número hasta el 1.
+		int n = 10;
+		
+		for(int i = n; i > 0; i--) {	//O(n)    	
+      System.out.println(i);      
+    }
+	}
+	
+}
+
+//Resultado de la ejecución
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
