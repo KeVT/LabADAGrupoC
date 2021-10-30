56bac8cba8e27e704c09b41cbbf90e20f10ad79f
KeVT <83045419+KeVT@users.noreply.github.com>
2021-10-30 18:13:04 -0500

    Create Q1lineal.java
    Dado un número, se imprimen números consecutivos desde el 0 las veces que representa ese número.
    Tiene una complejidad computacional de O(n).

---
+++ Laboratorio 5/Q1lineal.java
@@ -0,0 +1,22 @@
+//Q1lineal.java
+
+public class Q1lineal {
+
+	public static void main(String[] args) {	
+		//Dado un número, se imprimen números consecutivos las veces que representa ese número desde el 0 
+    Se imprime números consecutivos desde 0 hasta n
+		int n = 5;
+		
+		for(int i = 0; i < n; i++) {    //O(n)
+        	System.out.println(i);      
+        }	
+	}
+	
+}
+
+//Resultado de la ejecución
+0
+1
+2
+3
+4
