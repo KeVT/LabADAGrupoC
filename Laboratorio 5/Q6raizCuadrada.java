ae2ae42519ee542fc5e04d330b194e1f2b5c2f69
KeVT <83045419+KeVT@users.noreply.github.com>
2021-10-30 18:36:32 -0500

    Create Q6raizCuadrada.java
    Dado un número, se suma un número inicial en 0 con la cantidad de veces que va recorriendo el ciclo hasta ser menor o igual a dicho número.
    Tiene una complejidad computacional de O(n^½).

---
+++ Laboratorio 5/Q6raizCuadrada.java
@@ -0,0 +1,16 @@
+//Q6raizCuadrada.java
+
+public class Q6raizCuadrada {
+
+	public static void main(String[] args) {	
+		//Dado un número, se suma un número inicial en 0 con la cantidad de veces que va recorriendo el ciclo
+    //hasta ser menor o igual a dicho número.
+		int n = 10;
+		int p = 0;
+		for(int i = 1; p <= n; i++) {		//O(raíz(n))	
+			p = p + i;
+			System.out.println(p);
+		}		
+	}
+	
+}
