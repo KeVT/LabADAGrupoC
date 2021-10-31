4bae61de14222153fbc06dca63efe457c51a17d0
KeVT <83045419+KeVT@users.noreply.github.com>
2021-10-30 19:22:58 -0500

    Update Qlineal.java

--- Laboratorio 5/Q1lineal/Qlineal.java
+++ Laboratorio 5/Q1lineal/Qlineal.java
@@ -4,7 +4,6 @@ public class Q1lineal {
 
 	public static void main(String[] args) {	
 		//Dado un número, se imprimen números consecutivos las veces que representa ese número desde el 0 
-    Se imprime números consecutivos desde 0 hasta n
 		int n = 5;
 		
 		for(int i = 0; i < n; i++) {    //O(n)
