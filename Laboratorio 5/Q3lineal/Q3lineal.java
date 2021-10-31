85d4d3bc12230761e2610f5b6e6b74ad9962b861
KeVT <83045419+KeVT@users.noreply.github.com>
2021-10-30 19:33:22 -0500

    Update Q3lineal.java
    Dado un número, se imprimen números pares desde el 0 hasta antes de dicho número.
    Tiene una complejidad computacional de O(n).

--- Laboratorio 5/Q3lineal/Q3lineal.java
+++ Laboratorio 5/Q3lineal/Q3lineal.java
@@ -1,1 +1,23 @@
+//Q3lineal.java
 
+public class Q3lineal {
+
+	public static void main(String[] args) {	
+	  //Dado un número, se imprimen números pares desde el 0 hasta antes de dicho número.
+		int n = 12;
+		
+		for(int i = 0; i < n; i=i+2) {	//O(n)
+        	System.out.println(i);      
+        }
+		
+	}
+	
+}
+
+//Resultado de la ejecución
+0
+2
+4
+6
+8
+10
