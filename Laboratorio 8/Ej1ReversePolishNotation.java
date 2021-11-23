944e37b7b11be07e431734a8394e875ec5ff553d
KeVT <83045419+KeVT@users.noreply.github.com>
2021-11-23 01:34:59 -0500

    Create Ej1ReversePolishNotation.java
    Dado un arreglo de strings, realizar las operaciones básicas de acuerdo a la notación polaca inversa.

---
+++ Laboratorio 8/Ej1ReversePolishNotation.java
@@ -0,0 +1,54 @@
+//Ej1ReversePolishNotation.java
+
+import java.util.*;
+public class Ej1ReversePolishNotation {
+	
+	public static void main(String[] args) {
+
+		  String[] tokens = new String[] { "4", "13", "5", "/", "+" };
+		  System.out.println(evaluar(tokens));
+    
+	}
+	
+	public static int evaluar(String[] tokens) {
+		    //Dado un arreglo de strings, realizar las operaciones básicas de acuerdo a la notación polaca inversa
+        int returnValue = 0;
+ 
+        String operators = "+-*/";
+ 
+        Stack<String> stack = new Stack<String>();
+ 
+        for(String t : tokens){
+            if(!operators.contains(t)){
+                stack.push(t);
+            }
+            else{
+                int a = Integer.valueOf(stack.pop());
+                int b = Integer.valueOf(stack.pop());
+                int index = operators.indexOf(t);
+                switch(index){
+                    case 0:
+                        stack.push(String.valueOf(a+b));
+                        break;
+                    case 1:
+                        stack.push(String.valueOf(b-a));
+                        break;
+                    case 2:
+                        stack.push(String.valueOf(a*b));
+                        break;
+                    case 3:
+                        stack.push(String.valueOf(b/a));
+                        break;
+                }
+            }
+        }
+ 
+        returnValue = Integer.valueOf(stack.pop());
+        return returnValue;
+ 
+    }
+}
+
+//Resultado
+6
+
