30f93242fc208fc289f7f391c48fe19c6862cbfb
KeVT <83045419+KeVT@users.noreply.github.com>
2021-12-20 12:34:02 -0500

    Update Ej2TieRopes.java

--- Laboratorio 10/TieRopes/Ej2TieRopes.java
+++ Laboratorio 10/TieRopes/Ej2TieRopes.java
@@ -1,1 +1,14 @@
-sd
+class Solution {
+    public int solution(int K, int[] A) {
+        int resultado = 0;
+        int length = 0;
+        for (int rope : A) {
+            length += rope;
+            if (length >= K) {
+                resultado++;
+                length = 0;
+            }
+        }
+        return resultado;
+    }
+}
